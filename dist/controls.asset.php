76ec6bdd3d01754399c0e0bd24a6057607d83890
Jamil Uddin <jamilbd07@gmail.com>
2022-02-06 16:08:17 +0600

    removed style handler inline

---
+++ dist/controls.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0abfa4d92d0a3229203659af0ea60cbc');
